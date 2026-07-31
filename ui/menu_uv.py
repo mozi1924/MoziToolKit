e0efbcc999af5f9375ea8b44c993b1ad5c5b25ae
mozi1924 <mozi1924@arasaka.ltd>
2026-07-31 23:00:24 +0800

    feat: add nested context menus for UV and mesh operations

--- ui/menu_uv.py
+++ ui/menu_uv.py
@@ -3,22 +3,40 @@ from ..operators.uv.op_scale_uv import MOZI_OT_scale_uv
 from ..operators.uv.op_select_transparent_faces import MOZI_OT_select_transparent_faces
 
 
-def menu_func(self, context):
+class MOZI_MT_uv_context_menu(bpy.types.Menu):
+    bl_label = "MoziToolKit"
+    bl_idname = "MOZI_MT_uv_context_menu"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator(MOZI_OT_scale_uv.bl_idname)
+        layout.operator(MOZI_OT_select_transparent_faces.bl_idname)
+
+
+def draw_uv_menu_func(self, context):
     self.layout.separator()
     self.layout.operator(MOZI_OT_scale_uv.bl_idname)
     self.layout.operator(MOZI_OT_select_transparent_faces.bl_idname)
 
 
+def draw_uv_context_menu_func(self, context):
+    self.layout.separator()
+    self.layout.menu("MOZI_MT_uv_context_menu", text="MoziToolKit", icon="TOOL_SETTINGS")
+
 
 def register():
     if hasattr(bpy.types, "IMAGE_MT_uvs"):
-        bpy.types.IMAGE_MT_uvs.append(menu_func)
+        bpy.types.IMAGE_MT_uvs.append(draw_uv_menu_func)
     if hasattr(bpy.types, "VIEW3D_MT_uvs"):
-        bpy.types.VIEW3D_MT_uvs.append(menu_func)
+        bpy.types.VIEW3D_MT_uvs.append(draw_uv_menu_func)
+    if hasattr(bpy.types, "IMAGE_MT_uvs_context_menu"):
+        bpy.types.IMAGE_MT_uvs_context_menu.append(draw_uv_context_menu_func)
 
 
 def unregister():
-    if hasattr(bpy.types, "IMAGE_MT_uvs"):
-        bpy.types.IMAGE_MT_uvs.remove(menu_func)
+    if hasattr(bpy.types, "IMAGE_MT_uvs_context_menu"):
+        bpy.types.IMAGE_MT_uvs_context_menu.remove(draw_uv_context_menu_func)
     if hasattr(bpy.types, "VIEW3D_MT_uvs"):
-        bpy.types.VIEW3D_MT_uvs.remove(menu_func)
+        bpy.types.VIEW3D_MT_uvs.remove(draw_uv_menu_func)
+    if hasattr(bpy.types, "IMAGE_MT_uvs"):
+        bpy.types.IMAGE_MT_uvs.remove(draw_uv_menu_func)
