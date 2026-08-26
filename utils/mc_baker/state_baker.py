5ae405ea7315da16c29a8c4398c5ee084ed97e35
mozi1924 <mozi1924@arasaka.ltd>
2026-08-26 19:31:12 +0800

    fix: correct UV mapping coordinates for latch faces and chest east side in state_baker.py

--- utils/mc_baker/state_baker.py
+++ utils/mc_baker/state_baker.py
@@ -445,10 +445,10 @@ class StateBaker:
                 "west": {"texture": tex, "uv": [29, 33, 43, 43], "uv_size": 64},
             }
             latch_faces = {
-                "north": {"texture": tex, "uv": [1, 1, 2, 5], "uv_size": 64},
-                "south": {"texture": tex, "uv": [3, 1, 4, 5], "uv_size": 64},
-                "up": {"texture": tex, "uv": [1, 0, 2, 1], "uv_size": 64},
-                "down": {"texture": tex, "uv": [2, 0, 3, 1], "uv_size": 64},
+                "north": {"texture": tex, "uv": [3, 1, 4, 5], "uv_size": 64},
+                "south": {"texture": tex, "uv": [1, 1, 2, 5], "uv_size": 64},
+                "up": {"texture": tex, "uv": [2, 0, 3, 1], "uv_size": 64},
+                "down": {"texture": tex, "uv": [1, 0, 2, 1], "uv_size": 64},
                 "west": {"texture": tex, "uv": [0, 1, 1, 5], "uv_size": 64},
             }
         elif chest_type == "right":
@@ -478,7 +478,7 @@ class StateBaker:
                 "south": {"texture": tex, "uv": [1, 1, 2, 5], "uv_size": 64},
                 "up": {"texture": tex, "uv": [2, 0, 3, 1], "uv_size": 64},
                 "down": {"texture": tex, "uv": [1, 0, 2, 1], "uv_size": 64},
-                "east": {"texture": tex, "uv": [0, 1, 1, 5], "uv_size": 64},
+                "east": {"texture": tex, "uv": [2, 1, 3, 5], "uv_size": 64},
             }
         else:
             lid_from = [1, 9, 1]
